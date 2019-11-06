1e68a340ca77362a3a60b8fe6a852ae0dd2678b9
mgancarczyk <mt.gancarczyk@gmail.com>
2019-11-06 16:40:53 +0100

    Add Frek

---
+++ Frekwencja/Frekwencja/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = Console.ReadLine().ToCharArray();
+            var count = new Dictionary<char, int>();
+
+            foreach (var c in chars)
+            {
+                if (!count.ContainsKey(c))
+                {
+                    count.Add(c, 1);
+                }
+                else
+                {
+                    count[c]++;
+                }
+            }
+
+            Console.WriteLine(count);
+
+            Console.WriteLine("a)");
+            foreach (var entry in count.OrderBy(c => c.Key))
+            {
+                Console.WriteLine(entry.Key);
+            }
+
+            Console.WriteLine("----------------");
+
+            Console.WriteLine("b)");
+            foreach (var entry in count.OrderByDescending(c => c.Value))
+            {
+                Console.WriteLine(entry.Key);
+            }
+        }
+    }
+}
