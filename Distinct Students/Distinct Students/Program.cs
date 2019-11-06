9536405801b1f68e730f0e26079e2c49a522013f
mgancarczyk <mt.gancarczyk@gmail.com>
2019-11-06 16:35:32 +0100

    Add Distinct Students

---
+++ Distinct Students/Distinct Students/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studenci = new SortedSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                studenci.Add(input);
+            }
+
+            Console.WriteLine(studenci.Count);
+            int index = 0;
+            foreach (string student in studenci)
+            {
+                Console.WriteLine($"{++index}. {student}");
+            }
+        }
+    }
+}
